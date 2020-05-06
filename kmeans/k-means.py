123e6f55ffa3402a1fd1eb7efd222150e880eb31
bekir23 <55850657+bekir23@users.noreply.github.com>
2020-05-06 14:35:10 +0300

    Update k-means.py

--- kmeans/k-means.py
+++ kmeans/k-means.py
@@ -1,31 +1,32 @@
+#import libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-
+#Read the CSV file and look at the first five rows of the data
 data=pd.read_csv("Wholesale customers data.csv")
 print(data.head())
 
+#See that there is a lot of variation in the magnitude of the data. Variables like Channel and Region have low magnitude whereas variables like Fresh, Milk, Grocery, etc.have a higher magnitude.
 print(data.describe())
-#we see that there is a lot of variation in the magnitude of the data. Variables like Channel and Region have low magnitude whereas variables like Fresh, Milk, Grocery, etc.have a higher magnitude.
-#Since K-Means is a distance-based algorithm, this difference of magnitude can create a problem. So let’s first bring all the variables to the same magnitude
 
-# standardizing the data
+#Since K-Means is a distance-based algorithm, this difference of magnitude can create a problem. So let’s first bring all the variables to the same magnitude
+#Standardizing the data
 scaler = StandardScaler()
 data_scaled = scaler.fit_transform(data)
 
-# statistics of scaled data
+#See statistics of scaled data
 print(pd.DataFrame(data_scaled).describe())
 
-# defining the kmeans function with initialization as k-means++
+#Defining the kmeans function with initialization as k-means++
 kmeans = KMeans(n_clusters=2, init='k-means++')
 
-# fitting the k means algorithm on scaled data
+#Fitting the k means algorithm on scaled data
 kmeans.fit(data_scaled)
 
-#The K-means algorithm aims to choose centroids that minimise the inertia, or within-cluster sum-of-squares criterion:
+#The K-means algorithm aims to choose centroids that minimise the inertia, or within-cluster sum-of-squares criterion
 print(kmeans.inertia_)
 
 #We got an inertia value of almost 2600. Now, let’s see how we can use the elbow curve to determine the optimum number of clusters in Python.
@@ -56,6 +57,7 @@ kmeans = KMeans(n_jobs = -1, n_clusters=6, init='k-means++')
 kmeans.fit(data_scaled)
 pred = kmeans.predict(data_scaled)
 
+#let’s look at the value count of points in each of the above-formed clusters
 frame = pd.DataFrame(data_scaled)
 frame['cluster'] = pred
 print(frame['cluster'].value_counts())
